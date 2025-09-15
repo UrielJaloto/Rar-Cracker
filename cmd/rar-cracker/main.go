e440380b46daa002f4f1763dcc785c00c26bfb3a
UrielJaloto <urieljaloto@gmail.com>
2025-09-15 05:02:04 -0300

    refactor: improve main to use new config_validation

--- cmd/rar-cracker/main.go
+++ cmd/rar-cracker/main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/UrielJaloto/Rar-Cracker/internal/config"
 )
 
 func main() {
-	config := config.ParseConfig()
+	appConfig := config.New()
 
-	if err := config.ValidateFields(); err != nil {
-		println(err.Error())
-		return
+	configWarnings, err := appConfig.Validate()
+
+	if len(configWarnings) > 0 {
+		fmt.Println("(WARNINGS):")
+
+		for _, warning := range configWarnings {
+			fmt.Printf("  %s\n", warning)
+		}
 	}
 
-	config.PrintFields()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "\n%s ", err)
+		os.Exit(1)
+	}
+
+	appConfig.PrintFields()
 }
